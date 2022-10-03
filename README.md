37c601550e930b5ed17af474118fd6b777fc1dbe
neato0079 <neato0079@gmail.com>
2022-10-02 19:56:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+- `command.js` sends a `POST` request to `discord bot`. This `POST` request includes authorization headers and data about the command.
+- `discord bot` is invoked by the `discord client` when a user inputs a slash command.
+- When invoked, `discord bot` makes a call to `API Gateway`.
+- `API Gateway` triggers a `Lambda` function. In this case `index.js` is ran.
+- `index.js` contains the logic to handle a slash command. 
+- `index.js` sends `API Gateway` the return value of the slash command logic. `API Gateway` sends this value to `discord bot`.
+- `discord bot` displays the returned values in the `discord client`
---
+++ lambda_bot/index.js
@@ -0,0 +1,78 @@
+const nacl = require('tweetnacl');
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
+
+exports.handler = async (event) => {
+  console.log(event)
+  console.log(event.headers)
+  // Checking signature (requirement 1.)
+  // Your public key can be found on your application in the Developer Portal
+  const PUBLIC_KEY = process.env.PUBLIC_KEY;
+  const signature = event.headers['x-signature-ed25519'];
+  const timestamp = event.headers['x-signature-timestamp'];
+  const strBody = event.body; // should be string, for successful sign
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + strBody),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(PUBLIC_KEY, 'hex')
+  );
+
+  if (!isVerified) {
+    console.log('401 CODE')
+    return {
+      statusCode: 401,
+      body: JSON.stringify('invalid request signature'),
+    };
+  }
+
+
+  // Replying to ping (requirement 2.)
+  const body = JSON.parse(strBody)
+  if (body.type == 1) {
+    console.log('200 CODE')
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ "type": 1 }),
+    }
+  }
+
+  // Handle /foo Command
+  if (body.data.name == 'foo') {
+    console.log('HAPPY RESPONSE')
+    return JSON.stringify({  // Note the absence of statusCode
+      "type": 4,  // This type stands for answer with invocation shown
+      "data": { "content": "bar" }
+    })
+  }
+
+  // Handle /jpcountdown Command
+  const currentDate = dayjs();
+  const tripStart = dayjs('2022-12-30 23:00');
+  const tripHasStarted = dayjs().isBefore(tripStart)
+
+  const countDown = () => {
+    if (!tripHasStarted) {
+      return 'Count down finished'
+    };
+    const daysLeft = tripStart.diff(currentDate, 'days')
+    return `${daysLeft + 1} day(s) left until Japan trip!`
+  }
+
+  if (body.data.name == 'jpcountdown')
+    return JSON.stringify({  // Note the absence of statusCode
+      "type": 4,  // This type stands for answer with invocation shown
+      "data": { "content": countDown() }
+    })
+
+
+  console.log('Reach end of file :(')
+  return {
+    statusCode: 404  // If no handler implemented for Discord's request
+  }
+};
+
+/*
+
+*/
