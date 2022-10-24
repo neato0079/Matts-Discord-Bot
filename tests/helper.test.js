8629ed9818702fca32c6358a39e746e4c27533d7
neato0079 <neato0079@gmail.com>
2022-10-24 06:38:05 -0700

    Successful test of daysAndWeeksLeft function

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -3,3 +3,13 @@ const {
     countDown,
     currentExchangeRate
 } = require('../lambda_bot/helper');
+
+describe('daysAndWeeksLeft', () => {
+
+    test('Returns time in only weeks if the number of days until the trip is divisible by seven.' , () => {
+        // set up mocks
+        const daysLeft = 14
+
+        expect(daysAndWeeksLeft(daysLeft)).toBe('2 week(s) left until fly me to Japan!')
+    })
+})
