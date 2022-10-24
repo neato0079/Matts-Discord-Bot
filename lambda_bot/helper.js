ac4c349f518b7e69c77d91b7830267836ac6575e
neato0079 <neato0079@gmail.com>
2022-10-24 04:10:34 -0700

    First attempt at retry axios implementation

--- lambda_bot/helper.js
+++ lambda_bot/helper.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const retry = require('retry');
 const dayjs = require('dayjs');
 const relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(relativeTime);
@@ -7,9 +8,6 @@ const currentTime = dayjs().subtract(7, 'h');
 const tripStart = dayjs('2022-12-30 00:00');
 const tripHasStarted = dayjs().isBefore(tripStart);
 const daysLeft = tripStart.diff(currentTime, 'days') + 1;
-
-// console.log(`Current date/time: ${currentTime.format('MM/DD/YYYY HH:mm')}`)
-// console.log('Trip start:', tripStart.format('MM/DD/YYYY HH:mm'))
 
 const daysAndWeeksLeft = () => {
   const weeks = Math.floor(daysLeft / 7);
@@ -20,8 +18,6 @@ const daysAndWeeksLeft = () => {
   return `${weeks} week(s) and ${days} day(s) left until fly me to Japan!`
 }
 
-// console.log(daysAndWeeksLeft())
-
 const countDown = () => {
   if (!tripHasStarted) {
     return 'Count down finished'
@@ -30,17 +26,27 @@ const countDown = () => {
 
 }
 
-// console.log(countDown())
+// set up retry config
+const operation = retry.operation({
+  retries: 3,
+  factor: 3,
+  minTimeout: 1 * 1000,
+  maxTimeout: 60 * 1000,
+  randomize: true,
+});
 
 const currentExchangeRate = async () => {
-  // get it from an API and return it lol
-  const url = 'https://api.exchangerate.host/convert?from=USD&to=JPY';
-  // const exchangeRateConfig = {
-  //   base: 'USD'
-  // }
-  const response = await axios.get(url)
-  const USDtoJPY = response.data.result
-  return `1 USD = ${USDtoJPY.toFixed(2)} JPY`
+  operation.attempt(async (currentAttempt) => {
+    console.log('sending request: ', currentAttempt, ' attempt');
+    try {
+      const url = 'https://api.exchangerate.host/convert?from=USD&to=JPY';
+      const response = await axios.get(url)
+      const USDtoJPY = response.data.result
+      return `1 USD = ${USDtoJPY.toFixed(2)} JPY`
+    } catch (e) {
+      if (operation.retry(e)) { return; }
+    }
+  });
 }
 
 module.exports = {
