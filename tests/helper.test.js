1bbc07a854097016972c12f85caff94ec8aa96a5
Matt Quiba <neato0079@gmail.com>
2022-10-25 06:40:08 -0700

    Progress on currentExchangeRate test

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -4,12 +4,33 @@ const {
     currentExchangeRate
 } = require('../lambda_bot/helper');
 
+const axios = require('axios').default;
+
+jest.mock('axios');
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
 describe('daysAndWeeksLeft', () => {
 
     test('Returns time in only weeks if the number of days until the trip is divisible by seven.' , () => {
         // set up mocks
         const daysLeft = 14
-
-        expect(daysAndWeeksLeft(daysLeft)).toBe('2 week(s) left until fly me to Japan!')
+        // test and assert
+        expect(daysAndWeeksLeft(daysLeft)).toBe('2 week(s) left until fly me to Japan!');
     })
-})
+});
+
+describe('currentExchangeRate', () => {
+
+    test.skip('Returns USD to JPY exchange rate', () => {
+        // set up mocks
+        axios.get.mocked
+    })
+
+    test('Retries get request 3 times', () => {
+
+    })
+
+});
