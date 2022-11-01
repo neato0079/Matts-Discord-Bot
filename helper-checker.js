f9a4474a212a9f953443a747c7a6a45f2d12e43e
neato0079 <neato0079@gmail.com>
2022-11-01 13:17:41 -0700

    Just notes. Can ignore

--- helper-checker.js
+++ helper-checker.js
@@ -3,18 +3,20 @@ const helper = require('./lambda_bot/helper')
 
  const getExchange = async () => {
     const USDtoJPY = await helper.currentExchangeRate() 
+    console.log(await helper.currentExchangeRate)
     return `1 USD = ${USDtoJPY} JPY`
-    // console.log(await helper.currentExchangeRate)
     // return await helper.currentExchangeRate()
 
  }
 
 const main = async () => {
-    const data = {
-        stuff: 'stuff',
-        result: await getExchange()
-    }
-    return data
+    // const data = {
+    //     stuff: 'stuff',
+    //     result: await getExchange()
+    // }
+    // return data
+    const result = await helper.currentExchangeRate()
+    console.log(await result) 
 }
 
-getExchange().then(console.log('DONE'))
+main()
