787aced7937d8a7ef3af8ae1a7cf676b2110860c
neato0079 <neato0079@gmail.com>
2023-02-21 12:51:35 -0800

    set up libraries and add comments on them

---
+++ lambda_bot/bot.py
@@ -0,0 +1,7 @@
+import os # this is a library that allows the code to interact with the local os. ie reading local file contents. kinda like fs in js
+
+# see more here: https://docs.python.org/3/library/os.html
+
+import discord # discord's python API: https://discordpy.readthedocs.io/en/stable/
+from dotenv import load_dotenv # allows us to interact with .env files
+
