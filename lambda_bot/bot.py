118b750b81007546654f0fbb51c04e979b04e391
neato0079 <neato0079@gmail.com>
2023-02-22 13:44:32 -0800

    complete successful connection to discord

--- lambda_bot/bot.py
+++ lambda_bot/bot.py
@@ -5,3 +5,14 @@ import os # this is a library that allows the code to interact with the local os
 import discord # discord's python API: https://discordpy.readthedocs.io/en/stable/
 from dotenv import load_dotenv # allows us to interact with .env files
 
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
